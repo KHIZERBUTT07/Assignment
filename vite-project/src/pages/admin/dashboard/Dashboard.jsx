ee27c98ce098addd3e20d22ea7c6f1f6a636cbc3
KHIZERBUTT07 <153557362+KHIZERBUTT07@users.noreply.github.com>
2024-07-29 04:45:11 +0500

    k

--- vite-project/src/pages/admin/dashboard/Dashboard.jsx
+++ vite-project/src/pages/admin/dashboard/Dashboard.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
-
 function Dashboard() {
-  return (
-    <div>Dashboard</div>
-)
-}
-
-export default Dashboard
+    return (
+      <div>Dashboard</div>
+  )
+  }
+  export default Dashboard
